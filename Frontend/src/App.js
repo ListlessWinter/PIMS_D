60820f07c3b1353c011d95f0367d0cdb83d1ff69
Vincent <131525503+ListlessWinter@users.noreply.github.com>
2025-05-12 22:42:41 +0800

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Home from "./pages/Profile";
 
 export default function App() {
   return (
